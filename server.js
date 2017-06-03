8c0836b47f3df48d47f9ec699d30ee5d8d55596a
venky252003 <venky25@gmail.com>
2017-06-03 22:33:21 +0530

    Some commit db connection

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/',
-    mongoURLLabel = "mongodb://127.0.0.1:27017/";
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
 ////127.0.0.1:27017/
 
